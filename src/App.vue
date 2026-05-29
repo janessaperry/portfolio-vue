ab481551a26a8e555987b90723c67dc66b3bcaf7
janessaperry <hello@janessaperry.com>
2026-05-28 17:32:55 -0700

    refactor layout and styling: update structure, header, and footer design

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,50 @@
 <script setup lang="ts">
+import { PhGithubLogo } from '@phosphor-icons/vue'
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="@/assets/jp-logo.svg" width="48" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/projects">Projects</RouterLink>
-      </nav>
-    </div>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/projects">Projects</RouterLink>
+    </nav>
   </header>
 
-  <RouterView />
+  <main>
+    <RouterView />
+  </main>
+
+  <footer>
+    <div class="socials">
+      <ph-github-logo size="32" />
+    </div>
+
+    <div class="credit">
+      <img alt="Vue logo" class="logo" src="@/assets/jp-logo.svg" width="24" />
+      <p>Designed & developed by Janessa Perry</p>
+    </div>
+  </footer>
 </template>
 
 <style scoped>
 header {
-
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+  width: 100vw;
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  gap: 2rem;
 }
 
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  font-size: 1rem;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
 }
 
 nav a.router-link-exact-active {
@@ -48,38 +57,34 @@ nav a.router-link-exact-active:hover {
 
 nav a {
   display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
 }
 
 nav a:first-of-type {
   border: 0;
 }
 
+main {
+  padding: 1rem;
+  flex-grow: 1;
+}
+
+footer {
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.socials,
+.credit {
+  display: flex;
+  gap: 1rem;
+}
+
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+  main {
+    padding: 1rem;
+    flex-grow: 1;
   }
 }
 </style>
