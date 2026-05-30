a9e4541821a76dad0370f749b7fbae6e56efe13d
janessaperry <hello@janessaperry.com>
2026-05-29 20:07:59 -0700

    update HomeView layout and styles; add work status section with new gradients and shadows

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,7 +8,9 @@ import TheWelcome from '../components/TheWelcome.vue'
       <h1>Hey there, I'm Janessa</h1>
       <p class="tagline">Designer Turned Developer · Creative Problem Solver · Forever Curious, Always Evolving</p>
 
-      <div>Available for work</div>
+      <div class="work-status-container">
+        <p class="work-status">Available for work</p>
+      </div>
 
       <p>
         I'm a web developer with a background in design, specializing in creating responsive,
@@ -38,6 +40,9 @@ import TheWelcome from '../components/TheWelcome.vue'
 }
 
 .intro {
+  & > * + * {
+    margin-top: 2rem;
+  }
 
 }
 
@@ -45,10 +50,41 @@ import TheWelcome from '../components/TheWelcome.vue'
   font-size: var(--font-size-lg);
 }
 
+.work-status-container{
+  background: linear-gradient(to top right,
+  rgba(149, 218, 197, 0.8),
+  rgba(0, 157, 255, 0.8),
+  rgba(252, 0, 255, 0.8)
+  );
+  border-radius: 9999px;
+  padding: 1px;
+  width: fit-content;
+}
+
+.work-status {
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  padding: 2px 0.75rem;
+  background-color: var(--color-surface-dark-alt);
+  border-radius: 9999px;
+
+  &::before {
+    content: "";
+    display: block;
+    width: 8px;
+    height: 8px;
+    background-color: #95DAC5;
+    border-radius: 9999px;
+  }
+}
+
 .project-card {
   padding: 1rem;
   background-color: var(--color-surface-dark);
   border-radius: 1rem;
+  box-shadow: inset 0 1px 2px 0 var(--color-shadow-inset-light), inset 0 2px 4px 0 var(--color-shadow-inset), inset 0
+  -2px 4px 0 var(--color-shadow-inset-dark);
 }
 
 @media (min-width: 1024px) {
